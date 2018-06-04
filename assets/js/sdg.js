1d1b93f2f75ac3bf0a0c5d4ba37c34b77fcc12df
Shane Porter <shane@freshclickmedia.com>
2018-06-04 11:37:34 +0100

    testing

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -2,15 +2,5 @@
 # Don't delete this line.
 ---
 
-{% include chartjs/rescaler.js %}
-{% include event.js %}
-{% include accessibility.js %}
-{% include indicatorDataStore.js %}
-{% include indicatorModel.js %}
-{% include mapView.js %}
-{% include indicatorView.js %}
-{% include indicatorController.js %}
-{% include search.js %}
-{% include reportingStatus.js %}
-{% include menu.js %}
+
 
